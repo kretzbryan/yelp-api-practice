6343584b4bc54ff570c63fd49391406703aca3e8
bryan kretz <kretzbryan@gmail.com>
2021-01-27 16:11:47 -0800

    dependencies added, Stack Navigation added to App.js

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SearchScreen = () => {
+	return (
+		<View>
+			<Text>Search Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+export default SearchScreen;
